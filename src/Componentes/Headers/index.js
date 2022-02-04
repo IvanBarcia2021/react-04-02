f23b9b54e5aa3cb9553f801e3d97c58d25c04226
IvanBarcia2021 <ivo.barciagolato@gmail.com>
2022-02-04 10:06:12 -0300

    1 binaries uploaded

--- src/Componentes/Headers/index.js
+++ src/Componentes/Headers/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 export const Header = () => {
     return (
-        <div>
-          Header
+      <header>
+        <script src="https://unpkg.com/boxicons@2.1.1/dist/boxicons.js"></script>
+        <div className="Menu">
+          <box-icon name='menu'></box-icon>
+<a href="#"> 
+
+<div className="logo"> </div>
+<img src="" alt=""/>
+</a>
+ 
         </div>
+        </header>
         )}
--- src/Componentes/Productos/index.js
+++ src/Componentes/Productos/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 export const ProductosLista = () => {
     return (
         <div>
-            <h1>Ptoductos Lista</h1>
+            <h1>Productos Lista</h1>
         </div>
     )
 }
